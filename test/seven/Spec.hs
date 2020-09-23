b8696ccc43c41b67c1edc444aea18e3bde4acac8
Chris King-Parra <chris@kingparra.work>
2020-09-23 10:20:40 -0400

    Add test for ex7

---
+++ test/seven/Spec.hs
@@ -0,0 +1,17 @@
+import Test.Hspec (hspec, describe, it, shouldBe)
+import Test.QuickCheck
+import Seven (myFlatten, NestedList(..))
+
+main = hspec $ do
+  describe "myFlatten" $ do
+    it "A NestedList of three levels that contains elements\
+       \ 1..5 collectively will flatten to [1,2,3,4,5]" $ do
+      myFlatten ( List [ Elem 1
+                       , List [ Elem 2
+                              , List [ Elem 3
+                                     , Elem 4
+                                     ]
+                              , Elem 5
+                              ]
+                       ]
+                ) `shouldBe` [1,2,3,4,5]
