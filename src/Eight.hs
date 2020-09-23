d5ac101d95bc366b5940b3b83cea3bc71b1044b8
Chris King-Parra <chris@kingparra.work>
2020-09-23 13:52:05 -0400

    I find git emotionally distressing, sometimes

--- src/Eight.hs
+++ src/Eight.hs
@@ -2,7 +2,7 @@ module Eight (myCompress) where
 
 myCompress [] = []
 myCompress [x,y] = if x == y then [x] else [x,y]
-myCompress (x:y:xs) = 
+myCompress (x:y:xs) =
   if x == y
   then x : myCompress xs
   else x : y : myCompress xs
--- test/seven/Spec.hs
+++ test/seven/Spec.hs
@@ -6,12 +6,7 @@ main = hspec $ do
   describe "myFlatten" $ do
     it "A NestedList of three levels that contains elements\
        \ 1..5 collectively will flatten to [1,2,3,4,5]" $ do
-      myFlatten ( List [ Elem 1
-                       , List [ Elem 2
-                              , List [ Elem 3
-                                     , Elem 4
-                                     ]
-                              , Elem 5
-                              ]
-                       ]
-                ) `shouldBe` [1,2,3,4,5]
+      myFlatten (List [ Elem 1
+                  , List [ Elem 2
+                     , List [ Elem 3 , Elem 4 ]
+                     , Elem 5]]) `shouldBe` [1,2,3,4,5]
