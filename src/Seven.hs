159b01b11a179d9c9f1c5ef9879a6c8022c621a1
Chris King-Parra <chris@kingparra.work>
2020-09-23 14:21:21 -0400

    Implement ex7

--- src/Seven.hs
+++ src/Seven.hs
@@ -5,5 +5,5 @@ data NestedList a =
   deriving (Eq, Show)
 
 myFlatten :: NestedList a -> [a]
-myFlatten (Elem a) = [a] -- Base case, there is only one Elem.
-myFlatten (List a) = undefined -- Recursive case.. a is a list, I want to process it further...
+myFlatten (Elem a) = [a]
+myFlatten (List a) = concat . map myFlatten $ a
