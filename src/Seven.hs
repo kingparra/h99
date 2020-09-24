9c1bc156da6ba3a5efbd24eb14e86d7f5357b0c7
Chris King-Parra <chris@kingparra.work>
2020-09-24 15:04:03 -0400

    Add alternate versions of ex7

--- src/Seven.hs
+++ src/Seven.hs
@@ -1,9 +1,17 @@
-module Seven (myFlatten, NestedList(..)) where
+module Seven where
 
-data NestedList a = 
-  Elem a | List [NestedList a]
-  deriving (Eq, Show)
+data NestedList x = Elem x | List [NestedList x] deriving (Eq, Show)
 
-myFlatten :: NestedList a -> [a]
+myFlatten :: NestedList x -> [x]
 myFlatten (Elem a) = [a]
 myFlatten (List a) = concat . map myFlatten $ a
+
+myFlatten' :: NestedList x -> [x]
+myFlatten' (Elem a) = [a]
+myFlatten' (List a) = foldr ((++) . myFlatten') [] a
+
+-- I ended up cheating on this one :(
+myFlatten'' :: NestedList w -> [w]
+myFlatten'' (Elem a) = [a]
+myFlatten'' (List []) = []
+myFlatten'' (List (x:xs)) = myFlatten'' x ++ myFlatten'' (List xs)
--- test/seven/Spec.hs
+++ test/seven/Spec.hs
@@ -1,8 +1,9 @@
 import Test.Hspec (hspec, describe, it, shouldBe)
 import Test.QuickCheck
-import Seven (myFlatten, NestedList(..))
+import Seven
 
 main = hspec $ do
+
   describe "myFlatten" $ do
     it "A NestedList of three levels that contains elements\
        \ 1..5 collectively will flatten to [1,2,3,4,5]" $ do
@@ -10,3 +11,21 @@ main = hspec $ do
                   , List [ Elem 2
                      , List [ Elem 3 , Elem 4 ]
                      , Elem 5]]) `shouldBe` [1,2,3,4,5]
+
+  describe "myFlatten', a different implementation" $ do
+    it "A NestedList of three levels that contains elements\
+       \ 1..5 collectively will flatten to [1,2,3,4,5]" $ do
+      myFlatten' (List [ Elem 1
+                  , List [ Elem 2
+                     , List [ Elem 3 , Elem 4 ]
+                     , Elem 5]]) `shouldBe` [1,2,3,4,5]
+
+  describe "myFlatten''" $ do
+    it "A NestedList of three levels that contains elements 1..5 collectively will flatten to [1,2,3,4,5]" $ do
+      myFlatten'' (List [Elem 1, List [Elem 2, List [Elem 3 , Elem 4] , Elem 5]]) `shouldBe` [1,2,3,4,5]
+    it "will flatten hyiltiz' list" $ do
+      myFlatten'' (List [Elem 3, List [Elem 1, List [Elem 4, Elem 1, List [Elem 5]],Elem 9], Elem 2]) `shouldBe` [3,1,4,1,5,9,2]
+    it "will work for the simplest case, (Elem 1)" $ do
+      myFlatten'' (Elem 1) `shouldBe` [1]
+    it "will work when there is one level" $ do
+      myFlatten'' (List [Elem 1, Elem 2]) `shouldBe` [1,2]
