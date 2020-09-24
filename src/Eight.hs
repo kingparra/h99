d858d1a8c62f7df48b570f061d61ed13b9caba2e
Chris King-Parra <chris@kingparra.work>
2020-09-24 18:19:44 -0400

    Rewrite ex8 to use guards

--- src/Eight.hs
+++ src/Eight.hs
@@ -3,7 +3,6 @@ module Eight (myCompress) where
 myCompress :: Eq a => [a] -> [a]
 myCompress [] = []
 myCompress [x] = [x]
-myCompress (x:y:ys) =
-  if   x == y
-  then     myCompress (y:ys)
-  else x : myCompress (y:ys)
+myCompress (x:y:ys)
+  | x == y    = myCompress (y:ys)
+  | otherwise = x : myCompress (y:ys)
--- test/eight/Spec.hs
+++ test/eight/Spec.hs
@@ -3,9 +3,16 @@ import Eight (myCompress)
 
 main = hspec $ do
   describe "myCompress" $ do
-    it "myCompress \"aaabccaadeeee\" will return \"abcade\"" $ do
-      myCompress "aaabccaadeeee" `shouldBe` "abcade"
+
     it "myCompress [1] will return [1]" $ do
       myCompress [1] `shouldBe` [1]
+
     it "myCompress [1,2] will return [1,2]" $ do
       myCompress [1,2] `shouldBe` [1,2]
+
+    it "myCompress \"aaabccaadeeee\" will return \"abcade\"" $ do
+      myCompress "aaabccaadeeee" `shouldBe` "abcade"
+
+    it "myCompress \"sssylitherin loves  sssnakesss\" will\
+       \ return \"sylitherin loves snakes\"" $ do
+      myCompress "sssylitherin  loves  sssnakesss" `shouldBe` "sylitherin loves snakes"
