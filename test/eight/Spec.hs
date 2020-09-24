746255aef9e52917cef6835c6cc9fe12105afd6f
Chris King-Parra <chris@kingparra.work>
2020-09-24 18:08:01 -0400

    Add test cases for ex8

--- test/eight/Spec.hs
+++ test/eight/Spec.hs
@@ -5,3 +5,7 @@ main = hspec $ do
   describe "myCompress" $ do
     it "myCompress \"aaabccaadeeee\" will return \"abcade\"" $ do
       myCompress "aaabccaadeeee" `shouldBe` "abcade"
+    it "myCompress [1] will return [1]" $ do
+      myCompress [1] `shouldBe` [1]
+    it "myCompress [1,2] will return [1,2]" $ do
+      myCompress [1,2] `shouldBe` [1,2]
