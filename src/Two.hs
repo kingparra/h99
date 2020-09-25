48f2536496adda7b41d17d1824bf516855898470
Chris King-Parra <chris@kingparra.work>
2020-09-25 04:22:18 -0400

    Rewrite ex2
    Rewrite ex2 to use only
    pattern matching and the
    list constructors. (And
    error.)

--- src/Two.hs
+++ src/Two.hs
@@ -1,7 +1,6 @@
 module Two (myButLast) where
--- Problem 2
-myButLast :: [a] -> a
-myButLast l = case l of
-    []    -> error "empty list"
-    [a,_] -> a
-    _     -> l !! (length l - 2)
+
+myButLast         :: [a] -> a
+myButLast      []  =  error "empty list"
+myButLast   [a,_]  =  a
+myButLast  (_:xs)  =  myButLast xs
