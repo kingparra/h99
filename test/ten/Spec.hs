bb5894b066d3b863db426c2b500abe55ba7a0296
Chris King-Parra <chris@kingparra.work>
2020-10-23 13:28:18 -0400

    Add test for ex10

--- test/ten/Spec.hs
+++ test/ten/Spec.hs
@@ -6,3 +6,7 @@ main = hspec $ do
   describe "myEncode" $ do
     it "will get run-length encoding of sample string" $ do
       myEncode "aaaabccaadeeee" `shouldBe` [(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]
+    it "non-contiguous instances of the same character gets different counts" $ do
+      myEncode "ffffffxxxxxxaaaaaanononononono" `shouldBe` [(6,'f'),(6,'x'),(6,'a'),(1,'n'),
+        (1,'o'),(1,'n'),(1,'o'),(1,'n'),(1,'o'),(1,'n'),(1,'o'),(1,'n'),(1,'o'),(1,'n'),(1,'o')]
+
